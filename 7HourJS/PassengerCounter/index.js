2af0cd4281252d5390a198e896902698fa33a7f8
Susruth <up945169@myport.ac.uk>
2022-07-10 14:02:53 +0100

    Added save function with increment

--- 7HourJS/PassengerCounter/index.js
+++ 7HourJS/PassengerCounter/index.js
@@ -6,13 +6,19 @@
 //change the count-el in HTML to reflect the new count
 
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
+ 
 let count = 0;
 
 function increment() {
     console.log("Button was clicked"); 
-    count = count + 1;
+    count += 1;
     countEl.innerText = count;
-    console.log(count); 
-    
+}
+
+function save(){
+    let countStr = count + " - "
+
+    saveEl.innerText += countStr 
+    console.log(count)
 }
