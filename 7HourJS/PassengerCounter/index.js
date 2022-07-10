ddaac3598b8ee2e3400369b22a4aa2e2d5848308
Susruth <up945169@myport.ac.uk>
2022-07-10 13:02:46 +0100

    Finished Increment Counter

--- 7HourJS/PassengerCounter/index.js
+++ 7HourJS/PassengerCounter/index.js
@@ -1,1 +1,18 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+//initalise for 0
+//listen for clicks on button
+//increment the count variable 
+//change the count-el in HTML to reflect the new count
+
+let countEl = document.getElementById("count-el")
+
+let count = 0;
+
+function increment() {
+    console.log("Button was clicked"); 
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count); 
+    
+}
